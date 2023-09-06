09c24539d3528cb421efc80980b2896b871f0d51
PoojaBorad <poojabborad@gmail.com>
2023-09-06 15:36:27 -0700

    Update string_manipulation.py

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,4 +1,8 @@
 print("Create a program that takes a user's input (a sentence) and counts the number of words in it.\n")
 
-yourself = input("Tell me about yourself in few lines.")
+yourself = input("Tell me about yourself in a sentence: ").split()
+
+word_count = len(yourself)
+
+print(f"Number of words in your sentence : {word_count}")
 
