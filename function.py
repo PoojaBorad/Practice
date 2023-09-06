5d1dceb350ee51b2521e30c09aad64d309643617
PoojaBorad <poojabborad@gmail.com>
2023-09-06 15:22:52 -0700

    made another file
    used define in for loop

---
+++ function.py
@@ -0,0 +1,9 @@
+print("Write a function that calculates the area of a rectangle given its length and width as parameters.")
+
+def rectangle(length_width):
+    print(f"Area = {length_width}")
+
+
+w= float(input("Enter width of rectangle: ")) 
+l = float(input("Enter length of rectangle: "))
+rectangle (w*l)  
