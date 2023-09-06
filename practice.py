91044aaf69f84bf0881d0d9f0b4988200330151a
PoojaBorad <poojabborad@gmail.com>
2023-09-06 12:12:13 -0700

    Update practice.py
    Used if and else in for loop. Also, use the define function. Ask the choice of user.

--- practice.py
+++ practice.py
@@ -1,4 +1,17 @@
 #Practice of for loop!
+def fav_fruits(number):
+    print(f"Pick your favorite fruit from {number}")
+
 fruits = ["Mango", "Watermelon", "Apple", "Ice Apple", "Orange"]
+fav_fruits(fruits)
+
 for fruit in fruits:
-    print(fruit)
+     user_input =input(f"My favorite fruit is ").capitalize()
+     if user_input not in fruits:
+          print("Invalid choice, please pick from the list.")
+     else:
+          
+          break
+    
+
+     
