ca43dd026ec9d2f3d189b093bf09d8cb9154dd1a
PoojaBorad <poojabborad@gmail.com>
2023-09-07 13:51:24 -0700

    Update exception.py
    Using main function.

--- exception.py
+++ exception.py
@@ -1,13 +1,22 @@
 print("Write a program that asks the user for a number and handles exceptions (e.g., ValueError) if the user enters something that's not a number.")
 
-while True:
-    try:
-        number = float(input("Enter your choice of number: "))
-    except ValueError:
-        print("Invalid number. Write only number.")
-    else:
-        break
+def main():
+    number = only_number()
+    print(f"Your choice of number is {number}.")   
 
-print(f"Your choice of number is {number}.")            
+
+def only_number():
+    while True: 
+        try:
+            number = float(input("Enter your choice of number: "))
+        except ValueError:
+            print("Invalid number. Write only number.")
+        else:
+            break
+    return number    
+
+main()
+
+         
      
     
