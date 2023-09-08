4bf99393626fb9d0fe67f6a7641c7dc2a346b867
PoojaBorad <poojabborad@gmail.com>
2023-09-08 12:59:02 -0700

    Update list_and_sorting.py

--- list_and_sorting.py
+++ list_and_sorting.py
@@ -3,5 +3,5 @@ print("Create a list of numbers, sort them in ascending and descending order, an
 numbers = [5,23,17,65,1,98,125,365,47,12,2587,3698,14785,25631,985,425]
 print("List of the Numbers!", numbers)
 
-acesnding_order = 
-print("Let's sorti it out in Ascending Order!")
+acesnding_order = sorted(numbers)
+print("Let's sort it out in Ascending Order: ",acesnding_order)
