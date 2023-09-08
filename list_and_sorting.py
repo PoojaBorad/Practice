8b29a074b6bca844add5e94581a4c7a85e72ca0e
PoojaBorad <poojabborad@gmail.com>
2023-09-08 16:04:10 -0700

    Update list_and_sorting.py

--- list_and_sorting.py
+++ list_and_sorting.py
@@ -1,12 +1,12 @@
-print("Create a list of numbers, sort them in ascending and descending order, and print the results.\n")
+print("Create a list of numbers or names, sort them in ascending and descending order, and print the results.\n")
 
 #numbers = [5,23,17,65,1,98,125,365,47,12,2587,3698,14785,25631,985,425]
 #print("List of the Numbers!", numbers)
-user_input = input("Enter your list of numbers which you want to sort it out: ")
+user_input = input("Enter your list of numbers or names which you want to sort it out: ")
 
-numbers = [int(num) for num in user_input.split()]
+num_name = [list_a for list_a in user_input.split(',')]
 
-ascending_order = sorted(numbers)
+ascending_order = sorted(num_name)
 print("Let's sort it out in Ascending Order: ",ascending_order)
 
 descending_order = list(reversed(ascending_order))
